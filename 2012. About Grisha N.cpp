13c8d5be22981bebdd81044e9f247f97c5a87ba7
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-24 17:35:24 +0530

    Create 2012. About Grisha N.cpp

---
+++ 2012. About Grisha N.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair<int, int> ii;
+
+#define inf (int)1e9
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+    if(n >= 7) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
