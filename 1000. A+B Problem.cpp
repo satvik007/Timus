23685e0795521cbca97e3eeace607f6b05b873c3
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-24 17:10:49 +0530

    Create 1000. A+B Problem.cpp

---
+++ 1000. A+B Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair<int, int> ii;
+
+#define inf (int)1e9
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("in.txt", "r", stdin);
+   // freopen("out.txt", "w", stdout);
+
+    ll a, b;
+    cin >> a >> b;
+    cout << (a + b) << "\n";
+
+
+    return 0;
+}
