1676bd860b04c6ea69f8604e341ac4aed811fdd0
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-26 01:51:03 +0530

    Create 1457. Heating Main.cpp

---
+++ 1457. Heating Main.cpp
@@ -0,0 +1,35 @@
+#define BZ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using ll = int64_t;
+using ld = long double;
+using ull = uint64_t;
+using namespace std;
+using namespace __gnu_pbds;
+typedef vector <int> vi;
+
+const int INF = 1 << 30;
+
+
+
+int main() {
+#ifdef BZ
+    freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cout.setf(ios::fixed); cout.precision(20);
+
+    int n;
+    cin >> n;
+
+    double cnt = 0;
+    for(int i = 0; i < n; i++) {
+        int u; cin >> u;
+        cnt += u;
+    }
+
+    cnt /= n;
+
+    cout << fixed << setprecision(6) << cnt << "\n";
+
+}
