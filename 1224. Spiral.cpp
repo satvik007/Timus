25a083f08de5d3e849fd4f75d0449aec6ab7d21b
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-26 01:45:58 +0530

    Create 1224. Spiral.cpp

---
+++ 1224. Spiral.cpp
@@ -0,0 +1,28 @@
+#define BZ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using ll = int64_t;
+using ld = long double;
+using ull = uint64_t;
+using namespace std;
+using namespace __gnu_pbds;
+typedef vector <int> vi;
+
+const int INF = 1 << 30;
+
+
+
+int main() {
+#ifdef BZ
+    freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cout.setf(ios::fixed); cout.precision(20);
+
+    ll n, m;
+    cin >> n >> m;
+
+    cout << (min(2 * n - 2, 2 * m - 1)) << "\n";
+
+
+}
