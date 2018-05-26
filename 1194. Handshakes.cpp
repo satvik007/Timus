4165b791711b3fe805fe1d9f5804205ea1a523e3
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-26 19:43:20 +0530

    Create 1194. Handshakes.cpp

---
+++ 1194. Handshakes.cpp
@@ -0,0 +1,32 @@
+#define BZ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using ll = int64_t;
+using ld = long double;
+using ull = uint64_t;
+using namespace std;
+using namespace __gnu_pbds;
+typedef vector <int> vi;
+typedef pair <int, int> ii;
+
+const int INF = 1 << 30;
+
+
+
+int main() {
+#ifdef BZ
+    freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cout.setf(ios::fixed); cout.precision(20);
+
+
+
+    int n, k;
+    cin >> n >> k;
+
+    cout << (n * (n - 1) / 2 - k) << "\n";
+
+
+
+}
