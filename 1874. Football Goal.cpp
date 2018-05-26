0fdc05cc127728ac2bff48c3b8461bbb7b18789d
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2018-05-26 12:39:22 +0530

    Create 1874. Football Goal.cpp

---
+++ 1874. Football Goal.cpp
@@ -0,0 +1,31 @@
+#define BZ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using ll = int64_t;
+using ld = long double;
+using ull = uint64_t;
+using namespace std;
+using namespace __gnu_pbds;
+typedef vector <int> vi;
+typedef pair <int, int> ii;
+
+const int INF = 1 << 30;
+
+
+
+int main() {
+#ifdef BZ
+    freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cout.setf(ios::fixed); cout.precision(20);
+
+    double x, y;
+
+    cin >> x >> y;
+
+    cout << fixed << setprecision(6) << (x * x + y * y + 2 * x * y * sqrt(2)) / 4 << "\n";
+
+
+
+}
